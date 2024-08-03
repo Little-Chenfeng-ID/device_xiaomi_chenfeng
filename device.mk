064bc345570ea4c8b17ab27fa49515f31e1814fa
Karan Parashar <karan@pixelos.net>
2024-08-03 22:30:04 +0530

    chenfeng: Build libutils.vendor
    E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library "libutils.so" not found.
    
    Change-Id: I5bd9623fa7a25efd5c429bac3eca78e10855219e

--- device.mk
+++ device.mk
@@ -235,6 +235,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator-V1-ndk.vendor \
