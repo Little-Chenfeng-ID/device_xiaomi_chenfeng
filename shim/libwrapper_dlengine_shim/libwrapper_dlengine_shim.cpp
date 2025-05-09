3c634f04619c39b30925fdd0f33adf5ba2dfc31b
vherawidatama <vherawidatama@gmail.com>
2025-05-09 15:44:13 +0000

    chenfeng: just add symbol to solved camera's and add libmialgo_video_colourkeeping.so
    E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEBuilderC1EPNS_11DlContainer12IDlContainerE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.mawsaliency.so'
    E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEFactory16getTensorFactoryEv" referenced by "/odm/lib64/libmibokeh_845_video.so"..., on '/odm/lib64/camera/components/com.mi.node.videobokeh.so'
    E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: library "libmialgo_video_colourkeeping.so" not found: needed by /odm/lib64/camera/components/com.mi.node.rearvideo.so in namespace (default), on '/odm/lib64/camera/components/com.mi.node.rearvideo.so'
    
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
+++ shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
@@ -2,5 +2,6 @@
 
 extern "C" {
     void *_ZTVN3zdl8DlSystem21UserBufferEncodingTfNE = nullptr;
+    void *_ZN3zdl4SNPE11SNPEBuilderC1EPNS_11DlContainer12IDlContainerE = nullptr;
     void *_ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE = nullptr;
 }
