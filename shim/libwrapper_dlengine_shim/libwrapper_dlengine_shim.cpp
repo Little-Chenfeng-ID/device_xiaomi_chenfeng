84a1fd8c9211d123f53030a4bafdd26b37d00e59
vherawidatama <vherawidatama@gmail.com>
2025-05-09 00:05:57 +0000

    chenfeng: add symbols and import missing blobs
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
+++ shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
@@ -2,4 +2,5 @@
 
 extern "C" {
     void *_ZTVN3zdl8DlSystem21UserBufferEncodingTfNE = nullptr;
+    void *_ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE = nullptr;
 }
