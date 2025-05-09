26aa4123d0465eb07154fe23f9ef70290ec71cef
vherawidatama <vherawidatama@gmail.com>
2025-05-09 15:44:13 +0000

    chenfeng: just add symbol to solved camera's and add libmialgo_video_colourkeeping.so
    E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEBuilderC1EPNS_11DlContainer12IDlContainerE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.mawsaliency.so'
    E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEFactory16getTensorFactoryEv" referenced by "/odm/lib64/libmibokeh_845_video.so"..., on '/odm/lib64/camera/components/com.mi.node.videobokeh.so'
    E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: library "libmialgo_video_colourkeeping.so" not found: needed by /odm/lib64/camera/components/com.mi.node.rearvideo.so in namespace (default), on '/odm/lib64/camera/components/com.mi.node.rearvideo.so'
    
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- device.mk
+++ device.mk
@@ -54,7 +54,6 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
@@ -105,25 +104,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-# A2DP
-PRODUCT_PACKAGES += \
-    liba2dpoffload \
-    android.hardware.bluetooth.a2dp@1.0 \
-    android.hardware.bluetooth.a2dp@1.0.vendor \
-    android.hardware.bluetooth.a2dp@1.0-impl \
-    android.hardware.bluetooth.a2dp@1.0-service
- 
- PRODUCT_COPY_FILES += \
-     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-V3-ndk.vendor \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.bluetooth.audio@2.1.vendor
+    android.hardware.bluetooth@1.1.vendor
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth.audio-V1-ndk.vendor \
--- shim/libmibokeh_shim/libmibokeh_shim.cpp
+++ shim/libmibokeh_shim/libmibokeh_shim.cpp
@@ -2,5 +2,6 @@
 
 extern "C" {
     void *_ZN3zdl8DlSystem11TensorShapeD1Ev = nullptr;
+    void *_ZN3zdl4SNPE11SNPEFactory16getTensorFactoryEv = nullptr;
     void *_ZN3zdl4SNPE11SNPEFactory17getLibraryVersionEv = nullptr;
 }
