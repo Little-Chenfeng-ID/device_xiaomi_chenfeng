e6aca4ba98b939aace41f1ac7ab4886de5171567
vherawidatama <vherawidatama@gmail.com>
2025-05-10 12:39:15 +0000

    chenfeng: more changes about camera's and symbolic
    E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEBuilder24setExecutionPriorityHintENS_8DlSystem23ExecutionPriorityHint_tE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.mawsaliency.so'
    
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- device.mk
+++ device.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     libldacBT_abr \
-    libldacBT_bco \
+    libldacBT_bco:64 \
     libldacBT_enc
 
 PRODUCT_PACKAGES += \
--- shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
+++ shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
@@ -5,5 +5,6 @@ extern "C" {
     void *_ZN3zdl4SNPE11SNPEBuilder25setUseUserSuppliedBuffersEb = nullptr;
     void *_ZN3zdl4SNPE11SNPEBuilder19setRuntimeProcessorENS_8DlSystem9Runtime_tE = nullptr;
     void *_ZN3zdl4SNPE11SNPEBuilderC1EPNS_11DlContainer12IDlContainerE = nullptr;
+    void *_ZN3zdl4SNPE11SNPEBuilder24setExecutionPriorityHintENS_8DlSystem23ExecutionPriorityHint_tE = nullptr;
     void *_ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE = nullptr;
 }
