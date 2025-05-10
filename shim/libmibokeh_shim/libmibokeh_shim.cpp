3cb79efb31e0c9b81bd2999e8c0f33b36915df62
vherawidatama <vherawidatama@gmail.com>
2025-05-10 00:53:15 +0000

    chenfeng: patching again with symbolic E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEBuilder19setRuntimeProcessorENS_8DlSystem9Runtime_tE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.mawsaliency.so' E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEBuilderD1Ev" referenced by "/odm/lib64/libmibokeh_845_video.so"..., on '/odm/lib64/camera/components/com.mi.node.videobokeh.so' E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEBuilder19setRuntimeProcessorENS_8DlSystem9Runtime_tE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.dlengine.so'
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- shim/libmibokeh_shim/libmibokeh_shim.cpp
+++ shim/libmibokeh_shim/libmibokeh_shim.cpp
@@ -2,6 +2,7 @@
 
 extern "C" {
     void *_ZN3zdl8DlSystem11TensorShapeD1Ev = nullptr;
+    void *_ZN3zdl4SNPE11SNPEBuilderD1Ev = nullptr;
     void *_ZN3zdl4SNPE11SNPEFactory16getTensorFactoryEv = nullptr;
     void *_ZN3zdl4SNPE11SNPEFactory17getLibraryVersionEv = nullptr;
 }
