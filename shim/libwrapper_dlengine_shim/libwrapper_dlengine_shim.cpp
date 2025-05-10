8522e7edade9b26336bf6a16653fb41e89e485bb
vherawidatama <vherawidatama@gmail.com>
2025-05-10 03:52:19 +0000

    chenfeng: add symbols on libwrapper_dlengine
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
+++ shim/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
@@ -2,6 +2,7 @@
 
 extern "C" {
     void *_ZTVN3zdl8DlSystem21UserBufferEncodingTfNE = nullptr;
+    void *_ZN3zdl4SNPE11SNPEBuilder25setUseUserSuppliedBuffersEb = nullptr;
     void *_ZN3zdl4SNPE11SNPEBuilder19setRuntimeProcessorENS_8DlSystem9Runtime_tE = nullptr;
     void *_ZN3zdl4SNPE11SNPEBuilderC1EPNS_11DlContainer12IDlContainerE = nullptr;
     void *_ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE = nullptr;
